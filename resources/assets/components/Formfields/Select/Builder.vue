a4fcce722b4b49ca2f219f03a4a85d48787321b8
Christoph Schweppe <info@cschweppe.de>
2020-04-29 23:26:08 +0200

    Select-formfield list-options

--- resources/assets/components/Formfields/Select/Builder.vue
+++ resources/assets/components/Formfields/Select/Builder.vue
@@ -3,6 +3,47 @@
         <div v-if="show == 'list-options'">
             <label for="multiple" class="label">Multiple</label>
             <input type="checkbox" id="multiple" class="voyager-input" v-model="options.multiple">
+
+            <div class="w-full flex">
+                <div class="w-4/6">
+                    <h5>Options</h5>
+                </div>
+                <div class="w-2/6 text-right">
+                    <button class="button green small icon-only" @click.stop="addOption">
+                        <icon icon="plus" />
+                    </button>
+                </div>
+            </div>
+            <div class="voyager-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Key</th>
+                            <th>Value</th>
+                            <th>{{ __('voyager::generic.actions') }}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(option, i) in options.options" :key="'option-'+i">
+                            <td>
+                                <input type="text" class="voyager-input w-full" v-model="option.key" placeholder="Key">
+                            </td>
+                            <td>
+                                <language-input
+                                    class="voyager-input w-full"
+                                    type="text" placeholder="Value"
+                                    v-bind:value="option.value"
+                                    v-on:input="option.value = $event" />
+                            </td>
+                            <td>
+                                <button class="button red small icon-only" @click.stop="removeOption(key)">
+                                    <icon icon="trash" />
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div v-else-if="show == 'view-options'">
             <label class="label mt-4">Label</label>
